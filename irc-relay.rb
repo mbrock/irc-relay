3491838a6175ea4fd593cffb63e9c3db430e0a48
Mikael Brockman <phubuh@gmail.com>
2011-08-20 23:58:44 +0200

    Add some stuff that doesn't work.

--- irc-relay.rb
+++ irc-relay.rb
@@ -1,18 +1,65 @@
 # -*- coding: utf-8 -*-
 require 'gserver'
 
+Thread.abort_on_exception = true
+
+def returning(x)
+  yield; x
+end
+
+class IRCMessage
+  attr_accessor :prefix, :command, :params, :text
+
+  def initialize(prefix, command, params, text)
+    @prefix, @command, @params, @text = prefix, command, params, text
+  end
+  
+  def encode
+    p [command, @params]
+    if @params.respond_to? :join
+      puts 'wat?'
+      @params = @params.join(' ')
+    end
+    p @params
+    returning(s = "") do
+      s << ':' + prefix + ' ' unless prefix.nil?
+      s << command
+      s << ' ' + @params unless @params.empty?
+      s << ' :' + text unless text.nil?
+    end
+  end
+
+  def self.decode(s)
+    if s =~ /^(?:[:@]([^\s]+) )?([^\s]+)(?: ((?:[^:\s][^\s]* ?)*))?(?: ?:(.*))?$/
+      prefix, cmd, @params, text = $1, $2, $3, $4
+      prefix = nil if prefix.nil?
+      text = nil if text.nil?
+      new(prefix, cmd, @params, text)
+    end
+  end
+end
+
 class IRCServerConnection
-  def initialize(hostname, port)
+  def initialize(hostname, port, nickname, username, realname)
     Thread.new do
       @socket = TCPSocket.new(hostname, port)
+      puts "connected to #{hostname}"
+      send_command(nil, 'USER', [username, realname, '0', '0'], realname)
+      send_command(nil, 'NICK', nickname)
       loop do
         handle(@socket.gets)
       end
     end
   end
+
+  def send_command(prefix, command, params, text = nil)
+    s = IRCMessage.new(prefix, command, params, text).encode
+    puts s
+    @socket.puts s
+  end
   
   def handle(line)
-    puts line
+    p IRCMessage.decode(line)
   end
 end
 
@@ -21,8 +68,9 @@ class IRCRelayUser
     @connections = []
   end
 
-  def connect(hostname, port)
-    @connections << IRCServerConnection.new(hostname, port)
+  def connect(hostname, port, nickname, username, realname)
+    @connections << IRCServerConnection.new(hostname, port,
+                                            nickname, username, realname)
   end
 end
 
@@ -32,9 +80,13 @@ class IRCRelayServer < GServer
   end
 
   def serve(io)
-    @user = IRCRelayUser.new(io.gets)
+    name = io.gets
+    @user = IRCRelayUser.new(name)
     if io.gets =~ /^c (.*) (.*)$/
-      @user.connect $1, $2
+      puts "coooonnecting #$1 #$2"
+      @user.connect $1, $2, name, name, name
+    else
+      io.puts "WTF?"
     end
     io.puts "Välkommen till IRC Relay Server!"
     io.puts "Hoppas du trivs."
@@ -43,4 +95,5 @@ end
 
 relay_server = IRCRelayServer.new
 relay_server.audit = true
+relay_server.debug = true
 relay_server.start.join
