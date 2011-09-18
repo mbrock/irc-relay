614fb2969394260247b8888619e5475f8efccfa6
Niklas Dahr <niklas.dahr@gmail.com>
2011-09-18 19:08:39 +0200

    added /connect command

--- static/client.js
+++ static/client.js
@@ -1,15 +1,23 @@
 // The websocket server. 
 var ws = null;
+var username = null;
 
 // Outputs the message in the message div
 function outputMessage(string) {
     $("#messages").append("<div class=\"line\">"+string+"</div>");
 }
 
-function stringToJSON(string){
-  splitString = string.match("/(\w+)\s(.*)");
-  alert(splitString);
-  return splitString[0];
+function stringToCommand(string){
+    if(string.charAt(0) == '/'){
+        // Okay, this is a command
+        if(matches = string.match("/connect\\s([\\w\\.]+)\\s(\\d+)")){
+            return JSON.stringify({command:"connect", hostname: matches[1], port: matches[2]});
+        }
+    }else{
+        // If not command then display as chat-line
+        return JSON.stringify({"command":"send", "server":"irc.freenode.net", "message":{"command":"PRIVMSG", "text":msg, "params":["salkin"]}});
+    }
+
 }
 
 // Init websocket server on page load.
@@ -17,7 +25,7 @@ $(document).ready( function() {
     // Starts the socket
     var Socket = "MozWebSocket" in window ? MozWebSocket : WebSocket;
     ws = new Socket("ws://localhost:1337/");
-
+        
     // The callbacks
     ws.onmessage = function(evt) { 
         data = JSON.parse(evt.data);
@@ -30,23 +38,21 @@ $(document).ready( function() {
     ws.onclose = function() { outputMessage("Socket closed"); };
     ws.onopen = function() {
         outputMessage("Connected to backend.");
-        ws.send(JSON.stringify({command:"connect", hostname:"irc.freenode.net", port:6667}));
-        ws.send(JSON.stringify({command:"send", server:"irc.freenode.net", 
-                                message:{command:"USER", params:["mamaoeu","mamaoeu",0,0], text:"mamaoeu"}}));
+        
+//        ws.send(JSON.stringify({command:"send", server:"irc.freenode.net", 
+//                                message:{command:"USER", params:["mamaoeu","mamaoeu",0,0], text:"mamaoeu"}}));
 
     }
     
     // 
     $("#input_form").submit(function() {
       // Send the text from the input field on the webserver
-      // var msg = stringToJSON($("#input_text").val());
-    var msg = $("#input_text").val();
-    var command = {"command":"send",
-                   "server":"irc.freenode.net",
-                   "message":{"command":"PRIVMSG",
-                              "text":msg,
-                              "params":["salkin"]}}
-    ws.send(JSON.stringify(command));
+      var command = stringToCommand($("#input_text").val());
+        if(command){
+            ws.send(command);
+        }
+
+    // Push current input to history stack
     
     // Clear the field
     $("#input_text").val("");
