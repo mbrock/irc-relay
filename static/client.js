e4a28e92e0e5f57189295a5651fbc8bd2e09a8ca
Niklas Dahr <niklas.dahr@gmail.com>
2011-09-19 18:48:15 +0200

    add send history to webclient

--- static/client.js
+++ static/client.js
@@ -3,9 +3,14 @@ var ws = null;
 var username = "webbbottten";
 var servername = "irc.freenode.net";
 var channel = "#sholmtest";
+var history = new Array();
+var historyPos = 0;
+
 // Outputs the message in the message div
 function outputMessage(string) {
     $("#messages").append("<div class=\"line\">"+string+"</div>");
+    /// $("#messages").scrollTop());
+    
 }
 
 function stringToCommand(string){
@@ -25,38 +30,61 @@ function stringToCommand(string){
     // If not command then display as chat-line
     return JSON.stringify({command:"send", server:servername, message:{command:"PRIVMSG", text:string, "params":[channel]}});
 }
+
 // Init websocket server on page load.
 $(document).ready( function() {
     // Starts the socket
     var Socket = "MozWebSocket" in window ? MozWebSocket : WebSocket;
     ws = new Socket("ws://localhost:1337/");
-        
+    
     // The callbacks
     ws.onmessage = function(evt) { 
         data = JSON.parse(evt.data);
         outputMessage(data.text);
     };
     ws.onclose = function() { outputMessage("Socket closed"); };
-    ws.onopen = function() {
+    ws.onopen = function()  {
         outputMessage("Connected to backend.");
+        // Should download sent-history from backend
     }
     
     // 
-    $("#input_form").submit(function() {
-      // Send the text from the input field on the webserver
-      var command = stringToCommand($("#input_text").val());
-        if(command){
-            ws.send(command);
+    $("#input_form").submit( function() {
+        // Send the text from the input field on the webserver
+        var line = $("#input_text").val();
+        $("#input_text").val(""); // Clear the field
+        if(line != ""){
+            var command = stringToCommand(line);
+            if(command){
+                ws.send(command);
+            }
+            
+            // Push current input to history stack and set historyPos to point at the end
+            history.push(line);
+            historyPos = history.length;
+            
+            // Return false so the webpage does not reload because of form submit
+            return false;
         }
+    });
 
-    // Push current input to history stack
-    
-    // Clear the field
-    $("#input_text").val("");
+    $("#input_text").keydown( function(event) {
+        if(event.which == 38){
+            // Up
+            historyPos = Math.max(historyPos - 1, 0);
+            $("#input_text").val(history[historyPos]);
+            event.preventDefault();
+        }
+        else if(event.which == 40){
+            // Down
+            historyPos = Math.min(historyPos + 1, history.length);
+            // Make sure we can return to an empty line.
+            // TODO Maybe remember what we typed before pressing up.
+            $("#input_text").val( historyPos == history.length ? "" : history[historyPos]);
+            event.preventDefault();
+        }
+    });
 
-    // Return false so the webpage does not reload because of form submit
-    return false;
-  });
     $('#connect-button').button();    
     $('#connect-form')
         .dialog({ autoOpen: false,
